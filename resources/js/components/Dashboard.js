72ecc4d1860c6c6b5ffe0242222d92a095e06ebc
essamamdani <mamdani.info@gmail.com>
2020-04-22 14:31:19 +0500

    new update

--- resources/js/components/Dashboard.js
+++ resources/js/components/Dashboard.js
@@ -38,22 +38,22 @@ class Dashboard extends React.Component {
                                 <h4 className="pt_mn_page_hd">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                         <path fill="currentColor"
-                                              d="M12,3L2,12H5V20H19V12H22M9,18H7V12H9M13,18H11V10H13M17,18H15V14H17"></path>
+                                              d="M12,3L2,12H5V20H19V12H22M9,18H7V12H9M13,18H11V10H13M17,18H15V14H17"/>
                                     </svg>
                                     Dashboard
                                 </h4>
                                 <div className="tp_vid_fltr_tabs">
                                     <ul className="list-unstyled">
-                                        <li className={location.pathname.includes("dashboard")?"selected":""}>
-                                            <button onClick={e=>this.setState({tab:'dashboard'})}>Dashboard</button>
+                                        <li className={this.state.tab === 'dashboard' && 'selected'}>
+                                            <a href='#' onClick={e=>{e.preventDefault();this.setState({tab:'dashboard'})}}>Dashboard</a>
                                         </li>
-                                        <li className={location.pathname.includes("videos")?"selected":""}>
+                                        <li className={this.state.tab === 'videos' && 'selected'}>
                                             {/*<a href="#">Videos</a>*/}
-                                            <button onClick={e=>this.setState({tab:'videos'})}>Videos</button>
+                                            <a href='#' onClick={e=>{e.preventDefault();this.setState({tab:'videos'})}}>Videos</a>
                                         </li>
-                                        <li className={location.pathname.includes("comments")?"selected":""}>
+                                        <li className={this.state.tab === 'comments' && 'selected'}>
                                             {/*<Link to="/comments">Comments</Link>*/}
-                                            <button onClick={e=>this.setState({tab:'comments'})}>Comments</button>
+                                            <a href='#' onClick={e=>{e.preventDefault();this.setState({tab:'comments'})}}>Comments</a>
                                         </li>
                                     </ul>
                                 </div>
