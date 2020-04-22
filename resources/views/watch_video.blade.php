96e365f971c0ed085d79eb81d4a5db7bba9309fe
essamamdani <mamdani.info@gmail.com>
2020-04-22 19:17:44 +0500

    new update

--- resources/views/watch_video.blade.php
+++ resources/views/watch_video.blade.php
@@ -51,106 +51,106 @@
                                         </mediaelementwrapper>
 
                                     </div>
-                                    <div class="mejs__layers">
-                                        <div class="mejs__poster mejs__layer"
-                                             style="background-image: url({{asset("storage/$thumbnail")}}); width: 100%; height: 100%;">
-                                            <img class="mejs__poster-img" width="0" height="0"
-                                                 src="{{asset("storage/$thumbnail")}}"></div>
-                                        <div class="mejs__overlay mejs__layer"
-                                             style="display: none; width: 100%; height: 100%;">
-                                            <div class="mejs__overlay-loading"><span
-                                                    class="mejs__overlay-loading-bg-img"></span></div>
-                                        </div>
-                                        <div class="mejs__overlay mejs__layer"
-                                             style="display: none; width: 100%; height: 100%;">
-                                            <div class="mejs__overlay-error"></div>
-                                        </div>
-                                        <div class="mejs__overlay mejs__layer mejs__overlay-play"
-                                             style="width: 100%; height: 100%;">
-                                            <div class="mejs__overlay-button" role="button" tabindex="0"
-                                                 aria-label="Play" aria-pressed="false"></div>
-                                        </div>
-                                    </div>
-                                    <div class="mejs__controls">
-                                        <div class="mejs__button mejs__playpause-button mejs__play">
-                                            <button type="button" aria-controls="mep_0" title="Play" aria-label="Play"
-                                                    tabindex="0"></button>
-                                        </div>
-                                        <div class="mejs__time mejs__currenttime-container" role="timer"
-                                             aria-live="off"><span class="mejs__currenttime">00:00</span></div>
-                                        <div class="mejs__time-rail"><span class="mejs__time-total mejs__time-slider"
-                                                                           role="slider" tabindex="0"
-                                                                           aria-label="Time Slider" aria-valuemin="0"
-                                                                           aria-valuemax="NaN" aria-valuenow="0"
-                                                                           aria-valuetext="00:00"><span
-                                                    class="mejs__time-buffering" style="display: none;"></span><span
-                                                    class="mejs__time-loaded"></span><span
-                                                    class="mejs__time-current"></span><span
-                                                    class="mejs__time-hovered no-hover"></span><span
-                                                    class="mejs__time-handle"><span
-                                                        class="mejs__time-handle-content"></span></span><span
-                                                    class="mejs__time-float"><span class="mejs__time-float-current">00:00</span><span
-                                                        class="mejs__time-float-corner"></span></span></span></div>
-                                        <div class="mejs__time mejs__duration-container"><span class="mejs__duration">00:00</span>
-                                        </div>
-                                        <div class="mejs__button mejs__speed-button">
-                                            <button type="button" aria-controls="mep_0" title="Speed Rate"
-                                                    aria-label="Speed Rate" tabindex="0">1.00x
-                                            </button>
-                                            <div class="mejs__speed-selector mejs__offscreen">
-                                                <ul class="mejs__speed-selector-list">
-                                                    <li class="mejs__speed-selector-list-item"><input
-                                                            class="mejs__speed-selector-input" type="radio"
-                                                            name="mep_0_speed" value="2.00" id="mep_0-speed-2.00"><label
-                                                            for="mep_0-speed-2.00" class="mejs__speed-selector-label">2.00x</label>
-                                                    </li>
-                                                    <li class="mejs__speed-selector-list-item"><input
-                                                            class="mejs__speed-selector-input" type="radio"
-                                                            name="mep_0_speed" value="1.50" id="mep_0-speed-1.50"><label
-                                                            for="mep_0-speed-1.50" class="mejs__speed-selector-label">1.50x</label>
-                                                    </li>
-                                                    <li class="mejs__speed-selector-list-item"><input
-                                                            class="mejs__speed-selector-input" type="radio"
-                                                            name="mep_0_speed" value="1.25" id="mep_0-speed-1.25"><label
-                                                            for="mep_0-speed-1.25" class="mejs__speed-selector-label">1.25x</label>
-                                                    </li>
-                                                    <li class="mejs__speed-selector-list-item"><input
-                                                            class="mejs__speed-selector-input" type="radio"
-                                                            name="mep_0_speed" value="1.00" id="mep_0-speed-1.00"
-                                                            checked="checked"><label for="mep_0-speed-1.00"
-                                                                                     class="mejs__speed-selector-label mejs__speed-selected">1.00x</label>
-                                                    </li>
-                                                    <li class="mejs__speed-selector-list-item"><input
-                                                            class="mejs__speed-selector-input" type="radio"
-                                                            name="mep_0_speed" value="0.75" id="mep_0-speed-0.75"><label
-                                                            for="mep_0-speed-0.75" class="mejs__speed-selector-label">0.75x</label>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="mejs__button mejs__jump-forward-button">
-                                            <button type="button" aria-controls="mep_0" title="Jump forward 20 seconds"
-                                                    aria-label="Jump forward 20 seconds" tabindex="0">20
-                                            </button>
-                                        </div>
-                                        <div class="mejs__button mejs__volume-button mejs__mute">
-                                            <button type="button" aria-controls="mep_0" title="Mute" aria-label="Mute"
-                                                    tabindex="0"></button>
-                                            <a href="javascript:void(0);" class="mejs__volume-slider"
-                                               aria-label="Volume Slider" aria-valuemin="0" aria-valuemax="100"
-                                               role="slider" aria-orientation="vertical"><span class="mejs__offscreen">Use Up/Down Arrow keys to increase or decrease volume.</span>
-                                                <div class="mejs__volume-total">
-                                                    <div class="mejs__volume-current"
-                                                         style="bottom: 0px; height: 100%;"></div>
-                                                    <div class="mejs__volume-handle"
-                                                         style="bottom: 100%; margin-bottom: -3px;"></div>
-                                                </div>
-                                            </a></div>
-                                        <div class="mejs__button mejs__fullscreen-button">
-                                            <button type="button" aria-controls="mep_0" title="Fullscreen"
-                                                    aria-label="Fullscreen" tabindex="0"></button>
-                                        </div>
-                                    </div>
+{{--                                    <div class="mejs__layers">--}}
+{{--                                        <div class="mejs__poster mejs__layer"--}}
+{{--                                             style="background-image: url({{asset("storage/$thumbnail")}}); width: 100%; height: 100%;">--}}
+{{--                                            <img class="mejs__poster-img" width="0" height="0"--}}
+{{--                                                 src="{{asset("storage/$thumbnail")}}"></div>--}}
+{{--                                        <div class="mejs__overlay mejs__layer"--}}
+{{--                                             style="display: none; width: 100%; height: 100%;">--}}
+{{--                                            <div class="mejs__overlay-loading"><span--}}
+{{--                                                    class="mejs__overlay-loading-bg-img"></span></div>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mejs__overlay mejs__layer"--}}
+{{--                                             style="display: none; width: 100%; height: 100%;">--}}
+{{--                                            <div class="mejs__overlay-error"></div>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mejs__overlay mejs__layer mejs__overlay-play"--}}
+{{--                                             style="width: 100%; height: 100%;">--}}
+{{--                                            <div class="mejs__overlay-button" role="button" tabindex="0"--}}
+{{--                                                 aria-label="Play" aria-pressed="false"></div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="mejs__controls">--}}
+{{--                                        <div class="mejs__button mejs__playpause-button mejs__play">--}}
+{{--                                            <button type="button" aria-controls="mep_0" title="Play" aria-label="Play"--}}
+{{--                                                    tabindex="0"></button>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mejs__time mejs__currenttime-container" role="timer"--}}
+{{--                                             aria-live="off"><span class="mejs__currenttime">00:00</span></div>--}}
+{{--                                        <div class="mejs__time-rail"><span class="mejs__time-total mejs__time-slider"--}}
+{{--                                                                           role="slider" tabindex="0"--}}
+{{--                                                                           aria-label="Time Slider" aria-valuemin="0"--}}
+{{--                                                                           aria-valuemax="NaN" aria-valuenow="0"--}}
+{{--                                                                           aria-valuetext="00:00"><span--}}
+{{--                                                    class="mejs__time-buffering" style="display: none;"></span><span--}}
+{{--                                                    class="mejs__time-loaded"></span><span--}}
+{{--                                                    class="mejs__time-current"></span><span--}}
+{{--                                                    class="mejs__time-hovered no-hover"></span><span--}}
+{{--                                                    class="mejs__time-handle"><span--}}
+{{--                                                        class="mejs__time-handle-content"></span></span><span--}}
+{{--                                                    class="mejs__time-float"><span class="mejs__time-float-current">00:00</span><span--}}
+{{--                                                        class="mejs__time-float-corner"></span></span></span></div>--}}
+{{--                                        <div class="mejs__time mejs__duration-container"><span class="mejs__duration">00:00</span>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mejs__button mejs__speed-button">--}}
+{{--                                            <button type="button" aria-controls="mep_0" title="Speed Rate"--}}
+{{--                                                    aria-label="Speed Rate" tabindex="0">1.00x--}}
+{{--                                            </button>--}}
+{{--                                            <div class="mejs__speed-selector mejs__offscreen">--}}
+{{--                                                <ul class="mejs__speed-selector-list">--}}
+{{--                                                    <li class="mejs__speed-selector-list-item"><input--}}
+{{--                                                            class="mejs__speed-selector-input" type="radio"--}}
+{{--                                                            name="mep_0_speed" value="2.00" id="mep_0-speed-2.00"><label--}}
+{{--                                                            for="mep_0-speed-2.00" class="mejs__speed-selector-label">2.00x</label>--}}
+{{--                                                    </li>--}}
+{{--                                                    <li class="mejs__speed-selector-list-item"><input--}}
+{{--                                                            class="mejs__speed-selector-input" type="radio"--}}
+{{--                                                            name="mep_0_speed" value="1.50" id="mep_0-speed-1.50"><label--}}
+{{--                                                            for="mep_0-speed-1.50" class="mejs__speed-selector-label">1.50x</label>--}}
+{{--                                                    </li>--}}
+{{--                                                    <li class="mejs__speed-selector-list-item"><input--}}
+{{--                                                            class="mejs__speed-selector-input" type="radio"--}}
+{{--                                                            name="mep_0_speed" value="1.25" id="mep_0-speed-1.25"><label--}}
+{{--                                                            for="mep_0-speed-1.25" class="mejs__speed-selector-label">1.25x</label>--}}
+{{--                                                    </li>--}}
+{{--                                                    <li class="mejs__speed-selector-list-item"><input--}}
+{{--                                                            class="mejs__speed-selector-input" type="radio"--}}
+{{--                                                            name="mep_0_speed" value="1.00" id="mep_0-speed-1.00"--}}
+{{--                                                            checked="checked"><label for="mep_0-speed-1.00"--}}
+{{--                                                                                     class="mejs__speed-selector-label mejs__speed-selected">1.00x</label>--}}
+{{--                                                    </li>--}}
+{{--                                                    <li class="mejs__speed-selector-list-item"><input--}}
+{{--                                                            class="mejs__speed-selector-input" type="radio"--}}
+{{--                                                            name="mep_0_speed" value="0.75" id="mep_0-speed-0.75"><label--}}
+{{--                                                            for="mep_0-speed-0.75" class="mejs__speed-selector-label">0.75x</label>--}}
+{{--                                                    </li>--}}
+{{--                                                </ul>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mejs__button mejs__jump-forward-button">--}}
+{{--                                            <button type="button" aria-controls="mep_0" title="Jump forward 20 seconds"--}}
+{{--                                                    aria-label="Jump forward 20 seconds" tabindex="0">20--}}
+{{--                                            </button>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="mejs__button mejs__volume-button mejs__mute">--}}
+{{--                                            <button type="button" aria-controls="mep_0" title="Mute" aria-label="Mute"--}}
+{{--                                                    tabindex="0"></button>--}}
+{{--                                            <a href="javascript:void(0);" class="mejs__volume-slider"--}}
+{{--                                               aria-label="Volume Slider" aria-valuemin="0" aria-valuemax="100"--}}
+{{--                                               role="slider" aria-orientation="vertical"><span class="mejs__offscreen">Use Up/Down Arrow keys to increase or decrease volume.</span>--}}
+{{--                                                <div class="mejs__volume-total">--}}
+{{--                                                    <div class="mejs__volume-current"--}}
+{{--                                                         style="bottom: 0px; height: 100%;"></div>--}}
+{{--                                                    <div class="mejs__volume-handle"--}}
+{{--                                                         style="bottom: 100%; margin-bottom: -3px;"></div>--}}
+{{--                                                </div>--}}
+{{--                                            </a></div>--}}
+{{--                                        <div class="mejs__button mejs__fullscreen-button">--}}
+{{--                                            <button type="button" aria-controls="mep_0" title="Fullscreen"--}}
+{{--                                                    aria-label="Fullscreen" tabindex="0"></button>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
                                 </div>
                             </div>
 
