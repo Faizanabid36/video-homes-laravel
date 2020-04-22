b90f90108604095d50bbe7404ed5909e942c4c31
essamamdani <mamdani.info@gmail.com>
2020-04-22 16:30:36 +0500

    new update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -178,21 +178,21 @@
                         </a>
                         <ul class="dropdown-menu" role="menu">
                             <li>
-                                <a href="#/customize_player">
+                                <a href="#/profile">
                                     <img width="20"
-                                         src="{{asset('upload/photos/d-avatar.jpg')}}">{{auth()->user()->name}}
-                                    <div><span>Customize Player</span></div>
+                                         src="{{asset('upload/photos/d-avatar.jpg')}}">{{auth()->user()->name}}/>
+                                    {{--                                    <div><span>Customize Player</span></div>--}}
                                 </a>
                             </li>
-                            {{--                            <li>--}}
-                            {{--                                <a href="#">--}}
-                            {{--                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">--}}
-                            {{--                                        <path fill="currentColor"--}}
-                            {{--                                              d="M21,19V17H8V19H21M21,13V11H8V13H21M8,7H21V5H8V7M4,5V7H6V5H4M3,5A1,1 0 0,1 4,4H6A1,1 0 0,1 7,5V7A1,1 0 0,1 6,8H4A1,1 0 0,1 3,7V5M4,11V13H6V11H4M3,11A1,1 0 0,1 4,10H6A1,1 0 0,1 7,11V13A1,1 0 0,1 6,14H4A1,1 0 0,1 3,13V11M4,17V19H6V17H4M3,17A1,1 0 0,1 4,16H6A1,1 0 0,1 7,17V19A1,1 0 0,1 6,20H4A1,1 0 0,1 3,19V17Z"></path>--}}
-                            {{--                                    </svg>--}}
-                            {{--                                    Subscriptions--}}
-                            {{--                                </a>--}}
-                            {{--                            </li>--}}
+                            <li>
+                                <a href="#/customize_player">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                              d="M21,19V17H8V19H21M21,13V11H8V13H21M8,7H21V5H8V7M4,5V7H6V5H4M3,5A1,1 0 0,1 4,4H6A1,1 0 0,1 7,5V7A1,1 0 0,1 6,8H4A1,1 0 0,1 3,7V5M4,11V13H6V11H4M3,11A1,1 0 0,1 4,10H6A1,1 0 0,1 7,11V13A1,1 0 0,1 6,14H4A1,1 0 0,1 3,13V11M4,17V19H6V17H4M3,17A1,1 0 0,1 4,16H6A1,1 0 0,1 7,17V19A1,1 0 0,1 6,20H4A1,1 0 0,1 3,19V17Z"></path>
+                                    </svg>
+                                    Customize Player
+                                </a>
+                            </li>
                             <li>
                                 <a href="#/playlist">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
