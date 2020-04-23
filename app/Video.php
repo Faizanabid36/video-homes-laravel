699b2a46a35bf958da36eae29df2c02340acfbb2
essamamdani <mamdani.info@gmail.com>
2020-04-24 01:12:28 +0500

    new update

--- app/Video.php
+++ app/Video.php
@@ -4,11 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Video extends Model
-{
+class Video extends Model {
     //
     protected $guarded = [];
     protected $dates = [
         'converted_for_streaming_at',
     ];
+    protected $hidden = [ 'created_at',
+        'disk',
+        'updated_at',
+        'duration', 'original_name', 'size', 'video_id', 'video_path' ];
 }
