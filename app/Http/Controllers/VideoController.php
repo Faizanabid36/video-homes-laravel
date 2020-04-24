96579ec3fe3763c3cd5cc249763395b8fe8eb35b
essamamdani <mamdani.info@gmail.com>
2020-04-25 03:19:51 +0500

    new update

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -8,8 +8,7 @@ use Image;
 use App\Video;
 use App\User;
 
-class VideoController extends Controller
-{
+class VideoController extends Controller {
 
 
     public function upload_video( StoreVideoRequest $request ) {
@@ -18,8 +17,9 @@ class VideoController extends Controller
         request()->video->storeAs( 'public/uploads/', $file );
         $path          = 'uploads/' . $file;
         $media         = \FFMpeg::open( $path );
-        $dimension = $media->getStreams()->videos()->first()->getDimensions();
-        $newThumbnails = generateThumbnailsFromVideo( $media,$path, 3 );
+        $dimension     = $media->getStreams()->videos()->first()->getDimensions();
+        $newThumbnails = generateThumbnailsFromVideo( $media, $path, 3 );
+        dd($dimension->getWidth());
         $video         = Video::create( [
             'thumbnail'     => $newThumbnails[1],
             'original_name' => request()->video->getClientOriginalName(),
@@ -36,19 +36,19 @@ class VideoController extends Controller
             ConvertVideoForStreaming::dispatch( $video, 640, 360, [ '360p' => 1 ] );
         }
         if ( $this->width >= 854 ) {
-            ConvertVideoForStreaming::dispatch( $video, 854, 480, [ '480p' => 1 ] );
+            ConvertVideoForStreaming::dispatch( $video, 854, 480, [ '480p' => 1 ],1000 );
         }
         if ( $this->width >= 1280 ) {
-            ConvertVideoForStreaming::dispatch( $video, 1280, 720, [ '720p' => 1 ] );
+            ConvertVideoForStreaming::dispatch( $video, 1280, 720, [ '720p' => 1 ],1000 );
         }
         if ( $this->width >= 1920 ) {
-            ConvertVideoForStreaming::dispatch( $video, 1920, 1080, [ '1080p' => 1 ] );
+            ConvertVideoForStreaming::dispatch( $video, 1920, 1080, [ '1080p' => 1 ],1000 );
         }
         if ( $this->width >= 2560 ) {
-            ConvertVideoForStreaming::dispatch( $video, 2560, 1440, [ '2048p' => 1 ] );
+            ConvertVideoForStreaming::dispatch( $video, 2560, 1440, [ '2048p' => 1 ],1000 );
         }
         if ( $this->width >= 3840 ) {
-            ConvertVideoForStreaming::dispatch( $video, 3840, 2160, [ '4k' => 1 ] );
+            ConvertVideoForStreaming::dispatch( $video, 3840, 2160, [ '4k' => 1 ],1000 );
         }
         if ( $this->width >= 7680 ) {
             ConvertVideoForStreaming::dispatch( $video, 7680, 4320, [ '8k' => 1 ] );
@@ -59,31 +59,30 @@ class VideoController extends Controller
         return compact( 'message', 'video', 'newThumbnails' );
     }
 
-    public function watch_video($username)
-    {
-        $user = User::whereUsername($username)->first();
-        $video = Video::where('video_id', request('v'))->where('user_id', $user->id)->with('user')->first();
-        abort_if(!$video->processed, 201, 'Video Encoding is in process, Please wait a while');
-        $related_video = Video::whereUserId($user->id)->where('video_id', '!=', request('v'))
-            ->with('user')->latest()->first();
+    public function watch_video( $username ) {
+        $user  = User::whereUsername( $username )->first();
+        $video = Video::where( 'video_id', request( 'v' ) )->where( 'user_id', $user->id )->with( 'user' )->first();
+        abort_if( ! $video->processed, 201, 'Video Encoding is in process, Please wait a while' );
+        $related_video = Video::whereUserId( $user->id )->where( 'video_id', '!=', request( 'v' ) )
+                              ->with( 'user' )->latest()->first();
+
 //        return compact('video', 'related_video');
-        return view('watch_video', compact('video', 'related_video'));
+        return view( 'watch_video', compact( 'video', 'related_video' ) );
 
     }
 
-    public function list_of_videos()
-    {
-        $videos = Video::latest()->with('user')->get();
-        return compact('videos');
+    public function list_of_videos() {
+        $videos = Video::latest()->with( 'user' )->get();
+
+        return compact( 'videos' );
     }
 
-    public function update_video(Video $video)
-    {
+    public function update_video( Video $video ) {
         //dd($video);
 
         return [
-            'status' => $video->update(request(['description', 'title', 'thumbnail'])),
-            'data' => request()->all()
+            'status' => $video->update( request( [ 'description', 'title', 'thumbnail' ] ) ),
+            'data'   => request()->all()
         ];
     }
 }
