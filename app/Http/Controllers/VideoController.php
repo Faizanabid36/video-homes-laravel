93ad3e3148107e5b3b552a6db3b34ffc87fcabc8
essamamdani <mamdani.info@gmail.com>
2020-04-25 08:04:05 +0500

    new update

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -19,31 +19,31 @@ class VideoController extends Controller {
         $file = \Str::random( 16 ) . '.' . request()->video->getClientOriginalExtension();
         request()->video->storeAs( 'public/uploads/', $file );
         $path        = 'uploads/' . $file;
-        $media       = \FFMpeg::open( $path );
-        $videostream = $media->getStreams()->videos()->first();
-        if ( $rotation = getVideoRotation( $videostream ) ) {
-            switch ( $rotation ) {
-                case 270:
-                case - 270:
-                    $angle = RotateFilter::ROTATE_270;
-                    Log::info( "nahi mil raha hai ", [ "tes" => $angle ] );
-                    $media = $media->filters()->rotate( $angle );
-                    break;
-                case 180:
-                case - 180:
-                    $angle = RotateFilter::ROTATE_180;
-                    Log::info( "nahi mil raha hai ", [ "tes" => $angle ] );
-                    $media = $media->filters()->rotate( $angle );
-                    break;
-                case 90:
-                case - 90:
-                    $angle = RotateFilter::ROTATE_90;
-                    Log::info( "nahi mil raha hai ", [ "tes" => $angle ] );
-                    $media = $media->filters()->rotate( $angle );
-                    break;
-            }
-
-        }
+        $media       = \FFMpeg::open( $path )->filters()->rotate(RotateFilter::ROTATE_90);
+//        $videostream = $media->getStreams()->videos()->first();
+//        if ( $rotation = getVideoRotation( $videostream ) ) {
+//            switch ( $rotation ) {
+//                case 270:
+//                case - 270:
+//                    $angle = RotateFilter::ROTATE_270;
+//                    Log::info( "nahi mil raha hai ", [ "tes" => $angle ] );
+//                    $media = $media->filters()->rotate( $angle );
+//                    break;
+//                case 180:
+//                case - 180:
+//                    $angle = RotateFilter::ROTATE_180;
+//                    Log::info( "nahi mil raha hai ", [ "tes" => $angle ] );
+//                    $media = $media->filters()->rotate( $angle );
+//                    break;
+//                case 90:
+//                case - 90:
+//                    $angle = RotateFilter::ROTATE_90;
+//                    Log::info( "nahi mil raha hai ", [ "tes" => $angle ] );
+//                    $media = $media->filters()->rotate( $angle );
+//                    break;
+//            }
+//
+//        }
 
         $dimension     = $media->getStreams()->videos()->first()->getDimensions();
         $newThumbnails = generateThumbnailsFromVideo( $media, $path, 3 );
