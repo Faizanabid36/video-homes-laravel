8e64e9eba0c129d492bd17e4922b60480263b10b
essamamdani <mamdani.info@gmail.com>
2020-04-25 08:26:25 +0500

    new update

--- app/Helpers/strings.php
+++ app/Helpers/strings.php
@@ -66,7 +66,7 @@ if ( ! function_exists( 'generateThumbnailsFromVideo' ) ) {
         for ( $i = 1; $i <= $thumbnail_shots; $i ++ ) {
             $newThumbnail[ $i ] = str_replace( "." . request()->video->getClientOriginalExtension(), "-$i.png", $path );
             if ( $angle ) {
-                $media->filters()->rotate( $angle );
+                $media->filters()->rotate( $angle )->synchronize();
                 $media->getFrameFromSeconds( $seconds )->export()->save( $newThumbnail[ $i ] );
             } else {
                 $media->getFrameFromSeconds( $seconds )->export()->save( $newThumbnail[ $i ] );
