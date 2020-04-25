1adfa4808f50d7d5777c18542e6500d0bbf3aab5
essamamdani <mamdani.info@gmail.com>
2020-04-25 08:35:45 +0500

    new update

--- app/Helpers/strings.php
+++ app/Helpers/strings.php
@@ -65,13 +65,13 @@ if ( ! function_exists( 'generateThumbnailsFromVideo' ) ) {
         $newThumbnail    = [];
         for ( $i = 1; $i <= $thumbnail_shots; $i ++ ) {
             $newThumbnail[ $i ] = str_replace( "." . request()->video->getClientOriginalExtension(), "-$i.png", $path );
-            if ( $angle ) {
-                $frame = $media->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($seconds));
-                $frame->filters()->rotate( $angle )->save($newThumbnail[ $i ]);
-//                $media->getFrameFromSeconds( $seconds )->export()->filters()->rotate( $angle )->save( $newThumbnail[ $i ] );
-            } else {
+//            if ( $angle ) {
+//                $frame = $media->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($seconds));
+//                $frame->filters()->rotate( $angle )->save($newThumbnail[ $i ]);
+////                $media->getFrameFromSeconds( $seconds )->export()->filters()->rotate( $angle )->save( $newThumbnail[ $i ] );
+//            } else {
                 $media->getFrameFromSeconds( $seconds )->export()->save( $newThumbnail[ $i ] );
-            }
+//            }
 
             $seconds += $divide_result;
         }
