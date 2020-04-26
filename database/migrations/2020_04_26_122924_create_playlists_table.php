3c329ef170f14d557980aba9156feb5dca6b594c
essamamdani <mamdani.info@gmail.com>
2020-04-26 18:52:44 +0500

    new update

--- database/migrations/2020_04_26_122924_create_playlists_table.php
+++ database/migrations/2020_04_26_122924_create_playlists_table.php
@@ -17,6 +17,7 @@ class CreatePlaylistsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('description')->nullable();
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
