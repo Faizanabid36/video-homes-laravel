b342983f50c16d9ec55abf392c5dd2ded403ce3d
essamamdani <mamdani.info@gmail.com>
2020-04-26 17:36:09 +0500

    new update

---
+++ resources/views/errors/processing.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('container')
+    Video is in processing, Please wait a while. <br>
+    The page automatically reloads when the video would be encoded.
+@endsection
