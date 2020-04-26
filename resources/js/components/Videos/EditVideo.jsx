50c75315038d88f24c5840ff52ad4366c6216798
essamamdani <mamdani.info@gmail.com>
2020-04-27 01:04:44 +0500

    new update

--- resources/js/components/Videos/EditVideo.jsx
+++ resources/js/components/Videos/EditVideo.jsx
@@ -4,6 +4,8 @@ import React, { useCallback, useState,useEffect } from "react";
 
 export default function EditVideo(props){
     const [state, setState] = useState(false);
+    const [categories, setCategories] = useState(false);
+    const [manualupload, setManualupload] = useState(false);
     const [thumbnails, setThumbnails] = useState(false);
     const [index, setIndex] = useState(0);
     const handleSelect = useCallback((selectedIndex, e) => {
@@ -22,30 +24,59 @@ export default function EditVideo(props){
         axios.get(`edit_video/${props.match.params.id}`).then(({data})=>{
             setState({...data.video});
             setThumbnails(data.thumbnails);
+            setCategories(data.categories)
         })
     }, []);
 
     return <Container className="container main-content" id="main-container">
         <div id="container_content">
             <Row><Col xs={8} className="mx-auto">
-                <Carousel interval={null} activeIndex={index} onSelect={handleSelect}>
-                    {console.log('t', thumbnails)}
-                    {thumbnails && Object.values(thumbnails).map(v => {
-                            console.log(v);
-                            return <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src={window.VIDEO_APP.base_url + "/storage/" + v}
-                                />
+                <Form.Group controlId="manual_upload">
+                    <Form.Check
+                        type="switch"
+                        id="upload-switch"
+                        label="Upload Thumbnail"
+                        onChange={e=>setManualupload(!manualupload)}
+                    />
+                    {!manualupload ? <Carousel interval={null} activeIndex={index} onSelect={handleSelect}>
+                            {thumbnails && Object.values(thumbnails).map(v => {
+                                    console.log(v);
+                                    return <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src={window.VIDEO_APP.base_url + "/storage/" + v}
+                                        />
 
-                            </Carousel.Item>
-                        }
-                    )}
-                </Carousel>
+                                    </Carousel.Item>
+                                }
+                            )}
+                        </Carousel> :
+                        <Form.File id="upload-thumbnail" custom>
+                            <Form.File.Input onChange={e=>console.log(e)} />
+                            <Form.File.Label data-browse="Upload Thumbnail Image">
+                                Upload Thumbnail Image
+                            </Form.File.Label>
+                        </Form.File>}
+                </Form.Group>
+
                 <Form.Group controlId="title">
                     <Form.Label>Title</Form.Label>
                     <Form.Control placeholder="Title" defaultValue={state.title} onChange={e => {
                         state.title = e.target.value;
+                        setState(state);
+                    }}/>
+                </Form.Group>
+                <Form.Group controlId="description">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control placeholder="Description" defaultValue={state.description} onChange={e => {
+                        state.description = e.target.value;
+                        setState(state);
+                    }}/>
+                </Form.Group>
+                <Form.Group controlId="tags">
+                    <Form.Label>Tags</Form.Label>
+                    <Form.Control placeholder="Tags" defaultValue={state.tags} onChange={e => {
+                        state.tags = e.target.value;
                         setState(state);
                     }}/>
                 </Form.Group>
