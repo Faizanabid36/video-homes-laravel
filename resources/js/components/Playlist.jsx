971e9cdf01b70eb12d1cc447f53a05fb5330b328
essamamdani <mamdani.info@gmail.com>
2020-05-03 16:44:37 +0500

    new update

--- resources/js/components/Playlist.jsx
+++ resources/js/components/Playlist.jsx
@@ -1,5 +1,13 @@
-import { Button, Carousel, Col,Row, Container, Form } from "react-bootstrap";
-import React, { useCallback, useState,useEffect } from "react";
-export default function Playlist(props){
-    return <div>PlayList</div>;
+import { Button, Carousel, Col, Row, Container, Form } from "react-bootstrap";
+import React, { useCallback, useState, useEffect } from "react";
+
+export default function Playlist(props) {
+    return <Container className="container main-content" id="main-container">
+        <div id="container_content">
+            <Row>
+                <Col><h1>Playlist</h1>
+                <p>Xyx</p></Col>
+            </Row>
+        </div>
+    </Container>;
 }
