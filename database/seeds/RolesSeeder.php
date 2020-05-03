7cc0e974f2dde2d6c2034074fe6a73202c86a902
Faizan Abid <faizanabid36@gmail.com>
2020-05-03 18:11:26 +0500

    user roles

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\UserRole;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $roles=[
+            ['role'=>'admin'],
+            ['role'=>'realtor'],
+            ['role'=>'video provider'],
+        ];
+        foreach ($roles as $role)
+            UserRole::create($role);
+    }
+}
