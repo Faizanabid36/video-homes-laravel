3b2a0a7062ba50c9ab15330d8be8c9561ef3ac97
soban <sobanshams08@gmail.com>
2020-05-29 21:11:11 +0500

    /home is added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,10 @@
 </head>
 <body>
 <div class="container-fluid">
-<div class="row">
-  <div class="col">    
+<div class="row  m-0 p-0">
+  <div class="col ">    
    <!-- Nav       -->
+   <div class="">
    <nav class="navbar navbar-expand-lg navbar-light bg-light home-nav ">
   <a class="navbar-brand" href="#">
  
@@ -26,7 +27,7 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto links-home">
-      <li class="nav-item active">
+      <li class="nav-item ">
         <a class="nav-link" href="#">DIRECTORY <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
@@ -44,7 +45,7 @@
    
   </div>
 </nav>
-
+</div>
 <!-- Nav-end -->
 
 
@@ -56,7 +57,7 @@
 
     <div class="header-wrapper" > 
     <iframe width="1349" height="440"
-    class="dullbg"
+    
     frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" 
     src="https://www.youtube.com/embed/3iXYciBTQ0c?autoplay=1&amp;controls=0&amp;disablekb=1&amp;fs=0&amp;iv_load_policy=3&amp;loop=1&amp;modestbranding=1&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.videohomes.com&amp;widgetid=1">
 </iframe>
@@ -69,59 +70,105 @@
 
     </div>
 
-
-
-
   
 </div>
-
-<div class="row m-0 p-0 mr-auto ml-auto ">
+<!-- 
+<div class="row m-0 p-0 mr-auto ml-auto "> -->
     
-<div class="icons-boxes  ">
+<div class="icons-boxes ">
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/video-image-141px.jpeg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Videographers</h5>
-    <p class="card-text">A Platform for your business.</p>
-    <a href="#" class="btn btn-primary">LEARN MORE</a>
-  </div>
-</div>
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/video-image-141px.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Videographers</h5>
+                <p class="card-text">A Platform for your business.</p>
+                <a href="#" class="btn btn-primary">LEARN MORE</a>
+            </div>
+            </div>
 
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/Index-Card-jpeg.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Local Business Owners</h5>
-    <p class="card-text">You have a story..let our Marketing Pros tell it!</p>
-    <a href="#" class="btn btn-success">LEARN MORE</a>
-  </div>
-</div>
-  
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/House-Icon-Jpeg.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Real Estate Agents</h5>
-    <p class="card-text">Finally a fully integrated solution for production and distribution that puts you in control.</p>
-    <a href="#" class="btn btn-primary">LEARN MORE</a>
-  </div>
-</div>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/Index-Card-jpeg.jpg" alt="Card image cap">
+                    <div class="card-body">
+                    <h5 class="card-title">Local Business Owners</h5>
+                    <p class="card-text">You have a story..let our Marketing Pros tell it!</p>
+                    <a href="#" class="btn btn-success">LEARN MORE</a>
+                    </div>
+                </div>
+            
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/House-Icon-Jpeg.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Real Estate Agents</h5>
+                <p class="card-text">Finally a fully integrated solution for production and distribution that puts you in control.</p>
+                <a href="#" class="btn btn-warning">LEARN MORE</a>
+            </div>
+            </div>
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/video-image-141px.jpeg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Videographers</h5>
-    <p class="card-text">A Platform for your business.</p>
-    <a href="#" class="btn btn-primary">LEARN MORE</a>
-  </div>
-</div>
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top images"  src="http://videohomes.com/wp-content/uploads/2019/07/Rss-icon-jpeg.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Writers and Content Producers</h5>
+                <p class="card-text">Tap into our network of video pros and see what cool things you can accomplish.</p>
+                <a href="#" class="btn btn-dark">LEARN MORE</a>
+            </div>
+            </div>
      
   
   <div>
   
-
-  
-
 </div>
+
+
+
+        <div class="row  m-0 p-0 ">
+            <div class="col-12 m-0 p-0 "  >
+                <div class="joinUs">  
+        <span> <b> Join Us . </b>It will only take a minute </span>     <span>  <button class="btn "> GET STARTED TODAY </button>  </span>
+                </div>
+            </div>    
+        </div>
+
+        <div class="row sayHello m-0 p-0 ">
+            <div class="col-12 m-0 p-0 "  >
+                <div class="form-home">  
+
+                    <h1>  Say Hello !  </h1>
+                    <p> Please contact us for more inforamation               </p>
+
+                    <form class="myform"> 
+                    <div class="form-group">
+                            <label for="exampleInputEmail1">YOUR NAME(REQUIRED)</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name">
+                            
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">YOUR EMAIL (REQUIRED)</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                           
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">SUBJECT</label>
+                            <input type="TEXT" class="form-control" id="exampleInputPassword1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">YOUR MESSAGE</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                      
+                        <button type="submit" class="btn btn-primary">SEND</button>
+                    </form>
+                </div>
+
+            </div>    
+        </div>
+
+        <div class="row sayHello m-0 p-0 ">
+            <div class="col-12 m-0 p-0 "  >
+                <div class="footer-top">     </div>
+                <div class="footer">  <h3> © VideoHomes.com LLC 2020  </h3>    </div>
+            </div>
+        </div>   
+
 </div>
 <script src="{{ asset('js/app.js') }}"></script>    
 </body>
