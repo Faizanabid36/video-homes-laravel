2f2cee4de1a0e7c78452da12cb11557cd96fdce3
Faizan Abid <Faizanabid36@gmail.com>
2020-06-12 14:34:37 +0500

    q

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,6 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
         $user_tags=UserTags::all();
-        $user_tags=[];
         View::share(
             'user_tags', $user_tags
         );
