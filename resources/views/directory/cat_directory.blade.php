1c3d571b7b21c9cc693932d3c9032bc50455d368
Faizan Abid <Faizanabid36@gmail.com>
2020-06-27 18:17:06 +0500

    qwe

--- resources/views/directory/cat_directory.blade.php
+++ resources/views/directory/cat_directory.blade.php
@@ -48,7 +48,6 @@
         </div>
 
         <!-- 3rd row -->
-        {{dd($count_users)}}
         @if(isset($count_users)&&$count_users>0)
         <div class="row Category-Boxes ">
             @if(isset($directory) && $directory)
