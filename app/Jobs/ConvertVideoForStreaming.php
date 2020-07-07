3aeb960cbac19f6b2076577c8559103c24a5ce7a
essamamdani <mamdani.info@gmail.com>
2020-07-07 17:37:10 +0500

    new update

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -47,7 +47,7 @@ class ConvertVideoForStreaming implements ShouldQueue {
         Log::info(  "Essa Outside Angle",[$this->angle]);
         if ( $this->angle ) {
             Log::info("Essa Inside Angle",[$this->angle]);
-            $video->filters()->rotate( $this->getAngle($this->angle) );
+            $video->filters()->rotate( $this->angle );
         }
         $video->filters()->pad( new Dimension( $this->width, $this->height ) );
 
