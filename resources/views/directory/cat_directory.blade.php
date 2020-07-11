8becb4d81127d722668916529aac2057bdcb0a34
essamamdani <mamdani.info@gmail.com>
2020-07-11 19:45:50 +0500

    new update

--- resources/views/directory/cat_directory.blade.php
+++ resources/views/directory/cat_directory.blade.php
@@ -38,30 +38,30 @@
 
         <!-- 3rd row -->
         @if(count($users)>0)
-            <div class="row Category-Boxes ">
-                @if(isset($directory) && $directory)
-                    @foreach($tags as $tag)
-                        <div class="col-6 my-0"><span> <a
-                                    href="{{route('ex_directory_by_category',preg_replace('/\W|\_+/m', '-', $tag->role))}}"> {{ucfirst($tag->role)}}</a></span>
-                            <span
-                                class="float-right">{{$tag->account_types_count}}</span>
-                            <hr/>
-                        </div>
-                    @endforeach
-                @else
-                    @foreach($tags as $tag)
-                        @if(!isset($role_cat))
-                            <div class="col-6">
+        <div class="row Category-Boxes ">
+            @if(isset($directory) && $directory)
+                @foreach($tags as $tag)
+                    <div class="col-6 my-0"><span> <a
+                                href="{{route('ex_directory_by_category',preg_replace('/\W|\_+/m', '-', $tag->role))}}"> {{ucfirst($tag->role)}}</a></span>
+                        <span
+                            class="float-right">{{$tag->account_types_count}}</span>
+                        <hr/>
+                    </div>
+                @endforeach
+            @else
+                @foreach($tags as $tag)
+                    @if(!isset($role_cat))
+                        <div class="col-6">
                         <span>
                             <a href="{{route('directory_by_sub_category',[$role_slug,preg_replace('/\W|\_+/m', '-', $tag->name)])}}"> {{ucfirst($tag->name)}}
                             </a>
                         </span>
-                                <span
-                                    class="float-right">{{$tag->sub_roles_count}}</span>
-                                <hr/>
-                            </div>
-                        @else
-                            <div class="col-6">
+                            <span
+                                class="float-right">{{$tag->sub_roles_count}}</span>
+                            <hr/>
+                        </div>
+                    @else
+                        <div class="col-6">
                         <span>
                             @if(!isset($role_cat))
                                 <a href="{{route('directory_by_sub_category',[$role_slug,preg_replace('/\W|\_+/m', '-', $tag['name'])])}}"> {{ucfirst($tag['name'])}}
@@ -71,64 +71,63 @@
                                 </a>
                             @endif
                         </span>
-                                <span
-                                    class="float-right">{{$tag['sub_roles_count']}}</span>
-                                <hr/>
-                            </div>
-                        @endif
-                    @endforeach
-                @endif
-            </div>
-            <div id="map">
-            </div>
+                            <span
+                                class="float-right">{{$tag['sub_roles_count']}}</span>
+                            <hr/>
+                        </div>
+                    @endif
+                @endforeach
+            @endif
+        </div>
+        <div id="map">
+        </div>
 
-            <div class="row">
-                <!--The div element for the map -->
-                {{--            <div class="col-12 text-center border p-0 pt-3 ">--}}
-                {{--                <iframe width="100%" height="450"--}}
-                {{--                        src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"--}}
-                {{--                        frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a--}}
-                {{--                        href="https://www.maps.ie/draw-radius-circle-map/">Create radius map</a></iframe>--}}
-                {{--                <br/>--}}
-                {{--            </div>--}}
-            </div>
+        <div class="row">
+            <!--The div element for the map -->
+            {{--            <div class="col-12 text-center border p-0 pt-3 ">--}}
+            {{--                <iframe width="100%" height="450"--}}
+            {{--                        src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"--}}
+            {{--                        frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a--}}
+            {{--                        href="https://www.maps.ie/draw-radius-circle-map/">Create radius map</a></iframe>--}}
+            {{--                <br/>--}}
+            {{--            </div>--}}
+        </div>
 
-            <div class="row">
-                <div class="col-12 my-4 p-0">
-                    <div class="float-left">
-                        <h6 class="my-3"> Found<span class="h-8"> {{count($users)}} </span>listings </h6>
-                        <div class="dropdown">
-                            <button class="btn btn-primary" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                Sort By
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#">Date</a>
-                                <a class="dropdown-item" href="#"> New to Old</a>
-                                <a class="dropdown-item" href="#">Alpahabet</a>
-                            </div>
+        <div class="row">
+            <div class="col-12 my-4 p-0">
+                <div class="float-left">
+                    <h6 class="my-3"> Found<span class="h-8"> {{count($users)}} </span>listings </h6>
+                    <div class="dropdown">
+                        <button class="btn btn-primary" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
+                            Sort By
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Date</a>
+                            <a class="dropdown-item" href="#"> New to Old</a>
+                            <a class="dropdown-item" href="#">Alpahabet</a>
                         </div>
                     </div>
-                    <div class="float-right" style="position: relative;top: 38px;">
-
-                        <ul class="nav Custom-nav2 nav-pills mb-3" id="pills-tab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home"
-                                   role="tab"
-                                   aria-controls="pills-home" aria-selected="true"><i class="fas fa-list icon"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile"
-                                   role="tab"
-                                   aria-controls="pills-profile" aria-selected="false"><i
-                                        class="fas fa-th-large icon"></i></a>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
+                <div class="float-right" style="position: relative;top: 38px;">
 
-                <div class="col-12 p-0">
-                    <div class="tab-content Custom-Tab2" id="pills-tabContent">
+                    <ul class="nav Custom-nav2 nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home"
+                               role="tab"
+                               aria-controls="pills-home" aria-selected="true"><i class="fas fa-list icon"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile"
+                               role="tab"
+                               aria-controls="pills-profile" aria-selected="false"><i class="fas fa-th-large icon"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-12 p-0">
+                <div class="tab-content Custom-Tab2" id="pills-tabContent">
                 <span class="tab-pane fade show active  " id="pills-home" role="tabpanel"
                       aria-labelledby="pills-home-tab">
                     @foreach($users as $account_user)
@@ -189,11 +188,11 @@
                     @endforeach
 
                 </span>
-                    </div>
                 </div>
-
-
             </div>
+
+
+        </div>
         @else
             <h1>No Result Found</h1>
         @endif
@@ -218,19 +217,19 @@
     <script>
         function initMap() {
                 @foreach($users as $u)
-                @if(!is_null($u->user_extra->location_latitude)||!is_null($u->user_extra->location_longitude))
-            var uluru = {
-                    lat: {{$u->user_extra->location_latitude}},
-                    lng: {{$u->user_extra->location_longitude}}
-                };
-            @endif
+                    @if(!is_null($u->user_extra->location_latitude)||!is_null($u->user_extra->location_longitude))
+                        var uluru = {
+                                lat: {{$u->user_extra->location_latitude}},
+                                lng: {{$u->user_extra->location_longitude}}
+                        };
+                    @endif
                 @endforeach
-            if (uluru) {
-                mapArea = document.getElementById('map');
-                mapArea.style.width = "100%"
-                mapArea.style.height = "400px"
-                var map = new google.maps.Map(mapArea, {zoom: 11, center: uluru});
-            }
+                    if(uluru){
+                        mapArea=document.getElementById('map');
+                        mapArea.style.width="100%"
+                        mapArea.style.height="400px"
+                        var map = new google.maps.Map(mapArea, {zoom: 11, center: uluru});
+                    }
 
                 @foreach($users as $u)
 
