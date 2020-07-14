56af3d1fcf80d74fb51f327a0ffeab56604b0ead
essamamdani <mamdani.info@gmail.com>
2020-07-15 02:36:37 +0500

    new update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Video Home') }}</title>
+    <title>{{ config('app.name', 'VideoHome') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app1.js') }}" defer></script>
