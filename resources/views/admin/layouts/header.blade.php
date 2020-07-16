16dd12ec670b13fdeb790a14dff82b2747f34daf
essamamdani <mamdani.info@gmail.com>
2020-07-16 15:00:44 +0500

    new update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -16,13 +16,13 @@
                             <span class="fa fa-sort-down white_bg"></span>
                         </button>
                         <div class="dropdown-menu admire_admin">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            <a class="dropdown-item" href="{{ route("logout") }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 <i class="fa fa-lock"></i> {{ __('Logout') }}
                             </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                            <form id="logout-form" action="{{ route("logout") }}" method="POST"
                                   style="display: none;">
                                 @csrf
                             </form>
