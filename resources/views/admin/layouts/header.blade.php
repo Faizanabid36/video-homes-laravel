a4994def399d678399f4217d3000091eb0287641
essamamdani <mamdani.info@gmail.com>
2020-07-16 15:01:45 +0500

    new update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <!-- .navbar -->
     <nav class="navbar navbar-static-top">
         <div class="container-fluid m-0">
-            <a class="navbar-brand float-left" href="{{url('/}}">
+            <a class="navbar-brand float-left" href="{{url('/'}}">
                 <h4> VideoHomes</h4>
             </a>
             <div class="topnav dropdown-menu-right float-right">
@@ -10,19 +10,19 @@
                     <div class="user-settings no-bg">
                         <button type="button" class="btn btn-default no-bg micheal_btn"
                                 data-toggle="dropdown">
-                            <img src="{{asset("img/admin.jpg")}}"
+                            <img src="{{asset('img/admin.jpg')}}"
                                  class="admin_img2 img-thumbnail rounded-circle avatar-img"
                                  alt="avatar"> <strong>{{ Auth::user()->name }} </strong>
                             <span class="fa fa-sort-down white_bg"></span>
                         </button>
                         <div class="dropdown-menu admire_admin">
-                            <a class="dropdown-item" href="{{ route("logout") }}"
+                            <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 <i class="fa fa-lock"></i> {{ __('Logout') }}
                             </a>
 
-                            <form id="logout-form" action="{{ route("logout") }}" method="POST"
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                   style="display: none;">
                                 @csrf
                             </form>
