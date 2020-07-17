84a7b6d6b77cd84dca249f47254fc19e12365566
essamamdani <mamdani.info@gmail.com>
2020-07-18 03:28:36 +0500

    new update

--- routes/directory.php
+++ routes/directory.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'MainController@index')->name('home');
-Route::get('/directory/{level1?}/{level2?}', 'MainController@directory1')->name('directory_level');
+Route::get('/directory/{level1?}/{level2?}', 'MainController@directory1')->name('directory');
 //Route::get('/directory', 'MainController@directory')->name('directory');
 //Route::get('/directory/{role}','MainController@ex_directory_by_category')->name('ex_directory_by_category');
 //Route::get('/directory/{role}/{subrole}','MainController@directory_by_sub_category')->name('directory_by_sub_category');
