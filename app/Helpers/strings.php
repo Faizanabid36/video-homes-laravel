4ae94c35011a84f6f8f689a5a30e504f46210c4a
essamamdani <mamdani.info@gmail.com>
2020-07-18 03:50:22 +0500

    new update

--- app/Helpers/strings.php
+++ app/Helpers/strings.php
@@ -228,9 +228,6 @@ function grabUsers( $categories, $forceClear = false ) {
     foreach ( $categories as $val ) {
         if ( isset( $val['list'] ) && ! empty( $val['list'] ) ) {
             foreach ( $val['list'] as $user ) {
-                if(!is_array($user['user_id'])){
-                    dd($user['id']);
-                }
                 $user                           = $user->toArray();
                 $d                              = array_merge( $user['user_id'], [
                     'location_latitude'  => $user['location_latitude'],
