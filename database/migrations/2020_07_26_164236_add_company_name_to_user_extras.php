cd459df5e31f55fd2b9ce8d53ec477b8580458ec
essamamdani <mamdani.info@gmail.com>
2020-07-26 22:08:41 +0500

    new update

--- database/migrations/2020_07_26_164236_add_company_name_to_user_extras.php
+++ database/migrations/2020_07_26_164236_add_company_name_to_user_extras.php
@@ -10,12 +10,32 @@ class AddCompanyNameToUserExtras extends Migration
      * Run the migrations.
      *
      * @return void
+     * `avatar` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `address` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `phone` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `bio` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `phone2` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `company_logo` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `license` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `website_link` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
      */
+
     public function up()
     {
         Schema::table('user_extras', function (Blueprint $table) {
             //
+            $table->string('direct_phone')->nullable();
             $table->string('company_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('office_phone')->nullable();
+            $table->string('website')->nullable();
+            $table->string('license_no')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->string('company_logo')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('youtube')->nullable();
         });
     }
 
