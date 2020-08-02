6c8afb2d23f970b71ff3e67479ed6a37dd145e82
essamamdani <mamdani.info@gmail.com>
2020-08-03 02:28:26 +0500

    some update

--- resources/js/public.js
+++ resources/js/public.js
@@ -9,6 +9,6 @@ try {
 
     $('#findapro').submit(function(e) {
         e.preventDefault();
-        $(this).attr("action",location.toString()+"/"+$( this ).find("option:selected")).submit();
+        $(this).attr("action",window.location.toString()+"/"+$( this ).find("option:selected").val()).submit();
     })
 } catch (e) {}
