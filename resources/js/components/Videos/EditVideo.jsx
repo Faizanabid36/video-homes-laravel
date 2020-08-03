9edbca63d3fed07a6ceb4225a731d9a9f7241a77
essamamdani <mamdani.info@gmail.com>
2020-08-04 03:36:29 +0500

    some update

--- resources/js/components/Videos/EditVideo.jsx
+++ resources/js/components/Videos/EditVideo.jsx
@@ -17,7 +17,7 @@ export default function EditVideo(props) {
     const onUpdate = useCallback(e => {
         console.log(state);
         axios.put('update-video/' + state.id, {...state}).then(({data}) => {
-            window.location.href = window.VIDEO_APP.base_url + "/" + state.username + "/watch_video?v=" + state.video_id;
+            window.location.href = window.VIDEO_APP.base_url + "/u/" + state.username + "/" + state.video_id;
         })
     }, [state, thumbnails]);
     const deleteVideo = useCallback(e => {
