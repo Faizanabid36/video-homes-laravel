5d9d7dabdad6e6ade49a52e72d61c84c46033968
essamamdani <mamdani.info@gmail.com>
2020-08-03 22:39:44 +0500

    some update

--- resources/js/public.js
+++ resources/js/public.js
@@ -1,5 +1,5 @@
 try {
-    require('axios');
+    window.axios = require('axios');
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
     require('jquery-ui');
