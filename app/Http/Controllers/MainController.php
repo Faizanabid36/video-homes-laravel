780a6ebffc7e9eaff87a44759d112acc828a49f0
essamamdani <mamdani.info@gmail.com>
2020-08-03 17:05:42 +0500

    new update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -39,7 +39,7 @@ class MainController extends Controller {
             } );
         } );
         if ( request( 'category_id' ) ) {
-            $users = UserCategory::find(request('category_id'))->without('children')->get();
+            $users = UserCategory::without('children')->find(request('category_id'));
             return $users;
 //            $users         = collect( $users );
         }
