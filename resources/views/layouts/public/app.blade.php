2e3bc8e31237111be3a1972b0735184d822b4dc2
essamamdani <mamdani.info@gmail.com>
2020-08-04 16:57:11 +0500

    new update

--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link href="{{asset('css/public.css')}}" rel="stylesheet">
+    <link href="{{asset('css/public.css?version=')}}{{time()}}" rel="stylesheet">
 
     @yield('style')
     <script>
