e9f3b2824a285ea9e8203b75c2e90523e0e74bec
essamamdani <mamdani.info@gmail.com>
2020-08-04 15:04:59 +0500

    new update

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -197,6 +197,7 @@ class VideoController extends Controller
 
     public function update_video(Video $video)
     {
+        dd(request('tags'));
         return ['status' => $video->update(request(['description', 'title', 'thumbnail', 'video_type', 'tags','category_id']))];
     }
 
