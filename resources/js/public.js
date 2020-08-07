8cc436caf27b4a3d98ae82578c27dbe34e5180d0
essamamdani <mamdani.info@gmail.com>
2020-08-08 04:19:26 +0500

    some update

--- resources/js/public.js
+++ resources/js/public.js
@@ -61,7 +61,7 @@ try {
                     }
                 }, false);
                 media.addEventListener('loadedmetadata', function(){
-                    $(".mejs__controls").before("<img class='watermark_logo' src='http://3.21.2.114/img/cropped-VideoHomes-3.png'/>")
+                    $(".mejs__controls").before("<img class='position-absolute watermark_logo' src='http://3.21.2.114/img/cropped-VideoHomes-3.png'/>")
                 },false);
             },
         });
