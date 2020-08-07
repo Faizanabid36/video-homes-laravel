feb6a15c519aabbc2539953d014e5fe68c3d4b92
essamamdani <mamdani.info@gmail.com>
2020-08-08 04:22:18 +0500

    some update

--- resources/views/directory/single.blade.php
+++ resources/views/directory/single.blade.php
@@ -9,6 +9,10 @@
             height: 400px; /* The height is 400 pixels */
             width: 100%; /* The width is the width of the web page */
         }
+        .watermark_logo {
+            width:200px;
+            bottom:40px;
+        }
     </style>
 @endsection
 @section('content')
