037e4bc4ff8b3a219829b6898a1471322b3d3e95
essamamdani <mamdani.info@gmail.com>
2020-08-08 02:54:08 +0500

    new update

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,6 +2,7 @@
 
 use App\Page;
 use \App\UserCategory;
+use App\Video;
 use \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
 // Home
 Breadcrumbs::for('home', function ($trail) {
@@ -29,10 +30,22 @@ Breadcrumbs::for('directory', function ($trail, $level1 = null,$level2 = null) {
 Breadcrumbs::for('directory_by_username', function ($trail, $username,$video_id = null) {
     $trail->parent('home');
     $trail->push('Directory', route('directory'));
-//    if($level1){
-//        $trail->push(UserCategory::whereSlug($level1)->first()->name, route('directory', $level1));
-//    }
-//    if($level2){
-//        $trail->push(UserCategory::whereSlug($level2)->first()->name, route('directory', $level2));
-//    }
+    $video = Video::userVideos( $username, $video_id )->firstOrFail();
+    $category = UserCategory::find($video->user_extra->user_category_id);
+    $level1 = $category;
+    $level2 = false;
+    if($category->parent_id){
+        $level2 = $level1;
+        $level1 = UserCategory::find($category->parent_id);
+    }
+    if($level1){
+        $trail->push($level1->name, route('directory', $level1->slug));
+    }
+    if($level2){
+        $trail->push($level2->name, route('directory', $level2->slug));
+    }
+    $trail->push($video->user_extra->user_id->name, route('directory_by_username',$username));
+    if($video_id){
+        $trail->push($video->title, route('directory_by_username',$username,$video_id));
+    }
 });
