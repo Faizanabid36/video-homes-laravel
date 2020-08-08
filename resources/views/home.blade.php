3357747d8fb26b81a0db25c891fdf38fb4a15d3f
essamamdani <mamdani.info@gmail.com>
2020-08-09 02:53:18 +0500

    new update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
                         </div>
 
                     </div>
-                    <div class="row position-relative mt-5">
+                    <div class="row position-relative iconBoxParent">
                         <div class="card col shadow-lg p-1">
                             <img class="w-100 pt-4 px-2"
                                  src="{{asset('img/video-image-141px.jpeg')}}"
