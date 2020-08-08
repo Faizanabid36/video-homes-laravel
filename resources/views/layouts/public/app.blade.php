3b92bdd19954cf47286d5ef1e1899102bf00546a
essamamdani <mamdani.info@gmail.com>
2020-08-09 03:08:40 +0500

    new update

--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -84,7 +84,7 @@
     </div>
     @endif
 @yield('content')
-    <div class="container-fluid bg-black ">
+    <div class="container-fluid bg-dark ">
         <div class="row">
             <div class="col-12  border-primary text-white py-5 text-center" style="border-top: 5px solid;">
                 <h3> © VideoHomes.com LLC 2020 </h3>
