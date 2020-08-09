cf33a763912d14737472f275060f5417314c895a
essamamdani <mamdani.info@gmail.com>
2020-08-10 04:07:40 +0500

    new update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -42,7 +42,7 @@
                                 @foreach($users as $item)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
-                                        <td><img src="{{asset(is_null($item->avatar)?'images/blank.png':$item->avatar)}}" class="w-25" alt=""></td>
+                                        <td><img src="{{asset(is_null($item->avatar)?'images/blank.png':$item->avatar)}}" class="w-25 rounded-circle" alt=""></td>
                                         <td>{{ $item->name }}</td>
                                         <td>{{ $item->username }}</td>
                                         <td>{{ $item->email }}</td>
