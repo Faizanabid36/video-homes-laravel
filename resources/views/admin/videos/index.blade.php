ebd0b8f5995bf5cf6d0bd73a0f56807e6a0e689e
essamamdani <mamdani.info@gmail.com>
2020-08-10 03:46:33 +0500

    new update

--- resources/views/admin/videos/index.blade.php
+++ resources/views/admin/videos/index.blade.php
@@ -54,10 +54,10 @@
                                         <td>{{round((($video->size)/1024)/1024,2)}} MB</td>
                                         <td>{{$video->is_video_approved ? "Approved" : "Rejected"}} </td>
                                         <td>
-                                            <a href="{{ url('/admin/videos/' . $item->id) }}" title="View Video"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-{{--                                            <a href="{{ url('/admin/videos/' . $item->id . '/edit') }}" title="Edit Video"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>--}}
+                                            <a href="{{ url('/admin/videos/' . $video->id) }}" title="View Video"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+{{--                                            <a href="{{ url('/admin/videos/' . $video->id . '/edit') }}" title="Edit Video"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>--}}
 
-                                            <form method="POST" action="{{ url('/admin/videos' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                            <form method="POST" action="{{ url('/admin/videos' . '/' . $video->id) }}" accept-charset="UTF-8" style="display:inline">
                                                 {{ method_field('DELETE') }}
                                                 {{ csrf_field() }}
                                                 <button type="submit" class="btn btn-danger btn-sm" title="Delete Video" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
