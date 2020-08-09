60b65f5d5bad8517e5b95511bcf39b027fd1db77
essamamdani <mamdani.info@gmail.com>
2020-08-10 00:04:52 +0500

    new update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -33,4 +33,89 @@
             </div>
         </div>
     </nav>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+        <a class="navbar-brand mr-auto mr-lg-0" href="#">Offcanvas navbar</a>
+        <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="{{route('admin_panel')}}">
+                        <i class="fa fa-home"></i>
+                        <span class="link-title menu_hide">&nbsp;Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('pages.index')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">&nbsp;Pages
+
+                            </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('admin.list_user')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">&nbsp;Users
+
+                            </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('videos_for_approval')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">&nbsp;User's Videos</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('admin.videos_list')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">&nbsp;Videos List
+                        </span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="{{route('categories.index')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">Video&nbsp;Categories
+
+                        </span>
+                    </a>
+                </li>
+
+                {{--                <li class="">--}}
+                {{--                    <a href="{{route('user_tags')}}">--}}
+                {{--                        <i class="fa fa-tachometer"></i>--}}
+                {{--                        <span class="link-title menu_hide">&nbsp;Industry--}}
+                {{--                        </span>--}}
+                {{--                    </a>--}}
+                {{--                </li>--}}
+                <li class="nav-item">
+                    <a href="{{route('user-categories.index')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">&nbsp;Industry/Profession/Expertise
+                        </span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="{{action('ReportQueryController@index')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">&nbsp;Reported Messages
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{action('ReportQueryController@reported_videos')}}">
+                        <i class="fa fa-tachometer"></i>
+                        <span class="link-title menu_hide">&nbsp;Reported Videos
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </div>
