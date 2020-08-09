c75cbacc343b8035f7572c9c2ddebf8e7d87ddac
essamamdani <mamdani.info@gmail.com>
2020-08-10 00:05:13 +0500

    new update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -34,7 +34,7 @@
         </div>
     </nav>
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-        <a class="navbar-brand mr-auto mr-lg-0" href="#">Offcanvas navbar</a>
+        <a class="navbar-brand mr-auto mr-lg-0" href="#">{{env('APP_NAME')}}</a>
         <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
             <span class="navbar-toggler-icon"></span>
         </button>
