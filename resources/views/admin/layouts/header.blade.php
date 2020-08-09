f5aced21e2096038a01ec4336d018a26af5f6dde
essamamdani <mamdani.info@gmail.com>
2020-08-10 03:52:15 +0500

    new update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -10,7 +10,7 @@
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
                 <a class="nav-link" href="{{route('admin_panel')}}">
-                    <i class="fa fa-home"></i> Dashboard
+                    Dashboard
                 </a>
             </li>
             <li class="nav-item">
