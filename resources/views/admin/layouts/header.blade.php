42995377a7271d2aa2e790375937dc6654bcb944
essamamdani <mamdani.info@gmail.com>
2020-08-10 00:32:29 +0500

    new update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -9,58 +9,58 @@
         <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a href="{{route('admin_panel')}}">
+                    <a class="nav-link" href="{{route('admin_panel')}}">
                         <i class="fa fa-home"></i> Dashboard
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{route('pages.index')}}">
+                    <a class="nav-link" href="{{route('pages.index')}}">
                         <i class="fa fa-tachometer"></i> Pages
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{route('admin.list_user')}}">
+                    <a class="nav-link" href="{{route('admin.list_user')}}">
                         <i class="fa fa-tachometer"></i> Users
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{route('videos_for_approval')}}">
+                    <a class="nav-link" href="{{route('videos_for_approval')}}">
                         <i class="fa fa-tachometer"></i>
                         <span class="link-title menu_hide">&nbsp;User's Videos</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{route('admin.videos_list')}}">
+                    <a class="nav-link" href="{{route('admin.videos_list')}}">
                         <i class="fa fa-tachometer"></i> Videos List
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a href="{{route('categories.index')}}">
+                    <a class="nav-link" href="{{route('categories.index')}}">
                         <i class="fa fa-tachometer"></i> Video&nbsp;Categories
                     </a>
                 </li>
 
                 {{--                <li class="">--}}
-                {{--                    <a href="{{route('user_tags')}}">--}}
+                {{--                    <a class="nav-link" href="{{route('user_tags')}}">--}}
                 {{--                        <i class="fa fa-tachometer"></i>--}}
                 {{--                        <span class="link-title menu_hide">&nbsp;Industry--}}
                 {{--                        </span>--}}
                 {{--                    </a>--}}
                 {{--                </li>--}}
                 <li class="nav-item">
-                    <a href="{{route('user-categories.index')}}">
+                    <a class="nav-link" href="{{route('user-categories.index')}}">
                         <i class="fa fa-tachometer"></i> User Categories
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a href="{{action('ReportQueryController@index')}}">
+                    <a class="nav-link" href="{{action('ReportQueryController@index')}}">
                         <i class="fa fa-tachometer"></i> Reported Messages
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{action('ReportQueryController@reported_videos')}}">
+                    <a class="nav-link" href="{{action('ReportQueryController@reported_videos')}}">
                         <i class="fa fa-tachometer"></i> Reported Videos
                     </a>
                 </li>
@@ -91,3 +91,95 @@
         </div>
     </nav>
 </div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand mr-auto mr-lg-0" href="{{route('admin_panel')}}">{{env('APP_NAME')}}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('admin_panel')}}">
+                    <i class="fa fa-home"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('pages.index')}}">
+                    <i class="fa fa-tachometer"></i> Pages
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('admin.list_user')}}">
+                    <i class="fa fa-tachometer"></i> Users
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('videos_for_approval')}}">
+                    <i class="fa fa-tachometer"></i>
+                    <span class="link-title menu_hide">&nbsp;User's Videos</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('admin.videos_list')}}">
+                    <i class="fa fa-tachometer"></i> Videos List
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('categories.index')}}">
+                    <i class="fa fa-tachometer"></i> Video&nbsp;Categories
+                </a>
+            </li>
+
+            {{--                <li class="">--}}
+            {{--                    <a class="nav-link" href="{{route('user_tags')}}">--}}
+            {{--                        <i class="fa fa-tachometer"></i>--}}
+            {{--                        <span class="link-title menu_hide">&nbsp;Industry--}}
+            {{--                        </span>--}}
+            {{--                    </a>--}}
+            {{--                </li>--}}
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('user-categories.index')}}">
+                    <i class="fa fa-tachometer"></i> User Categories
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="{{action('ReportQueryController@index')}}">
+                    <i class="fa fa-tachometer"></i> Reported Messages
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{action('ReportQueryController@reported_videos')}}">
+                    <i class="fa fa-tachometer"></i> Reported Videos
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="{{asset('img/admin.jpg')}}"
+                         class="admin_img2 img-thumbnail rounded-circle avatar-img"
+                         alt="avatar"> <strong>{{ auth()->user()->name }} </strong>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        <i class="fa fa-lock"></i> {{ __('Logout') }}
+                    </a>
+{{--                    <a class="dropdown-item" href="#">Another action</a>--}}
+{{--                    <div class="dropdown-divider"></div>--}}
+{{--                    <a class="dropdown-item" href="#">Something else here</a>--}}
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+        <form id="logout-form" class="d-none" action="{{ route('logout') }}" method="POST"
+              >
+            @csrf
+        </form>
+    </div>
+</nav>
