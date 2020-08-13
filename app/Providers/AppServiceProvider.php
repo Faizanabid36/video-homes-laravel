9955ec2205f3774aa8c3308db4e6827c8c19310f
essamamdani <mamdani.info@gmail.com>
2020-08-14 02:57:47 +0500

    some update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Page;
+use App\Settings;
 use App\UserCategory;
 use App\UserRole;
 use Illuminate\Support\Facades\Schema;
@@ -32,28 +33,6 @@ class AppServiceProvider extends ServiceProvider
 
         Schema::defaultStringLength(191);
         if (! $this->app->runningInConsole()) {
-            $industries           = UserRole::where( 'role', '!=', 'admin' )->get();
-//            $user_parent_category = UserCategory::whereNull( 'parent_id' )->get();
-//            $roles_assoc          = $user_parent_category->groupBy( 'role_id' );
-//
-//            $user_child_category  = $user_parent_category->groupBy( 'parent_id' );
-//
-//
-//            View::share(
-//                'user_parent_category', $user_parent_category
-//            );
-//            View::share(
-//                'user_child_category', $user_child_category
-//            );
-//            View::share( 'roles', $industries );
-//            View::share(
-//                'roles_assoc', $roles_assoc
-//            );
-//
-            view::composer( 'directory.cat_directory', function ( $view ) use ( $industries ) {
-                $view->with( compact( 'industries' ) );
-            } );
-
 
             View::composer( 'layouts.public.app', function ( $view ) {
                 $pages = Page::whereIsPublic( 1 )->get();
