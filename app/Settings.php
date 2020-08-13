e294af1666f8a9ccd0a18d822d472ca8575bcf15
essamamdani <mamdani.info@gmail.com>
2020-08-14 02:21:56 +0500

    new update

--- app/Settings.php
+++ app/Settings.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Settings extends Model {
     //
     protected $guarded = [];
-    protected $cast = [ "box_1" => "array","box_2" => "array","box_3" => "array","box_4" => "array" ];
+    protected $casts = [ "box_1" => "array","box_2" => "array","box_3" => "array","box_4" => "array" ];
 }
