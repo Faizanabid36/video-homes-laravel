38b3be229504cc311318fe6ce9fe57929d4753da
essamamdani <mamdani.info@gmail.com>
2020-08-14 01:45:46 +0500

    new update

--- resources/views/embed_video.blade.php
+++ resources/views/embed_video.blade.php
@@ -6,12 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$video->title}} - {{env('APP_NAME')}}</title>
-    <style>
-        * {
-            padding: 0;
-            margin: 0;
-        }
-    </style>
+    <link rel="stylesheet" href="{{asset('public/style.css')}}">
     <script>
         window.VIDEO_APP = {
             base_url: '{{url('/')}}',
