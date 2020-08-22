c9bff28e70c8f8c626088d8db0fb38ca95e32aca
essamamdani <mamdani.info@gmail.com>
2020-08-22 21:48:01 +0500

    new update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -30,9 +30,9 @@ class MainController extends Controller {
 
     public function slug($page){
         $split = explode('/',$page);
-        dd("Directory",$page,preg_match('/^directory\//m',$page));
-        if(preg_match('/^directory\/?/m',$page)){
 
+        if(preg_match('/^directory\/?/m',$page)){
+            dd("Directory",$page,preg_match('/^directory\//m',$page));
             return redirect(route('directory', [$split[1] ?? null,$split[2] ?? null]));
         } elseif(preg_match('/^embed\//m',$page)){
             return redirect(route('embed_video', [$split[1] ?? null]));
