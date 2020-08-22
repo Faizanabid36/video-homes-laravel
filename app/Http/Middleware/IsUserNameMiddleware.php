003d735eca0819b11f94d0bac5b957d8b07611b1
essamamdani <mamdani.info@gmail.com>
2020-08-22 22:16:30 +0500

    new update

---
+++ app/Http/Middleware/IsUserNameMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Page;
+use App\Video;
+use Closure;
+
+class IsUserNameMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $video = Video::userVideos( $request->route()->parameter( 'username' ), $request->route()->parameter( 'video_id' ) )
+        if($video->count() > 0){
+            return $next($request);
+        }
+
+    }
+}
