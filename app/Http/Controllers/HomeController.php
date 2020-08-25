0413d8c445a3fce412076e435e3244b0724c53ca
essamamdani <mamdani.info@gmail.com>
2020-08-25 18:15:01 +0500

    new update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,7 +46,7 @@ class HomeController extends Controller {
 
     public function edit_user_profile( User $user ) {
         if ( request( 'currentPassword' ) ) {
-            if ( Hash::check( request( 'confirmPassword' ), $user->password ) && request( 'newPassword1' ) === request( 'newPassword2' )  ) {
+            if ( Hash::check( request( 'currentPassword' ), $user->password ) && request( 'newPassword1' ) === request( 'newPassword2' )  ) {
                 return $user->update( [ 'password' => Hash::make( request( 'newPassword1' ) ) ] );
             }
 
