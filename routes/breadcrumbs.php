429771e2c1947421e00ff4c5403abeb5c80c1544
Faizan Abid <Faizanabid36@gmail.com>
2020-11-16 20:34:26 +0500

    middleware fixes

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -11,47 +11,51 @@ use \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
 //    $trail->push( 'Home', route( 'home' ) );
 //} );
 
-Breadcrumbs::for( 'public.page', function ( $trail, $slug ) {
+Breadcrumbs::for('public.page', function ($trail, $slug) {
 //    $trail->parent( 'home' );
-    $trail->push( Page::viewPage( $slug )->first()->title, route( 'public.page', $slug ) );
-} );
+    $trail->push(Page::viewPage($slug)->first()->title, route('public.page', $slug));
+});
 
 // Home > Blog > [Category]
-Breadcrumbs::for( 'directory', function ( $trail, $level1 = null, $level2 = null ) {
+Breadcrumbs::for('directory', function ($trail, $level1 = null, $level2 = null) {
 //    $trail->parent( 'home' );
-    $trail->push( 'Directory', route( 'directory' ) );
-    if ( $level1 ) {
-        $trail->push( UserCategory::whereSlug( $level1 )->firstOrFail()->name, route( 'directory', $level1 ) );
+    $trail->push('Directory', route('directory'));
+    if ($level1) {
+        $trail->push(!is_null(UserCategory::whereSlug($level1)->firstOrFail()->name)
+            ? UserCategory::whereSlug($level1)->firstOrFail()->name : '',
+            route('directory', $level1));
     }
-    if ( $level2 ) {
-        $trail->push( UserCategory::whereSlug( $level2 )->firstOrFail()->name, route( 'directory', [ $level1, $level2 ] ) );
+    if ($level2) {
+        $trail->push(!is_null(UserCategory::whereSlug($level2)->firstOrFail()->name)
+            ?UserCategory::whereSlug($level2)->firstOrFail()->name:'',
+            route('directory', [$level1, $level2]));
     }
-} );
+});
 
-Breadcrumbs::for( 'directory_by_username', function ( $trail, $slug, $video_id = null ) {
+Breadcrumbs::for('directory_by_username', function ($trail, $slug, $video_id = null) {
 //    $trail->parent( 'home' );
-    if ( Page::viewPage( $slug )->count() > 0 ) {
-        $trail->push( Page::viewPage( $slug )->first()->title, route( 'directory_by_username', $slug ) );
+    if (Page::viewPage($slug)->count() > 0) {
+        $trail->push(Page::viewPage($slug)->first()->title, route('directory_by_username', $slug));
 
         return;
     }
-    $trail->push( 'Directory', route( 'directory' ) );
+    $trail->push('Directory', route('directory'));
 
-    $video    = Video::userVideos( $slug, $video_id )->first();
-    $user     = User::whereUsername( $slug )->first();
-    $category = UserCategory::find( $user->user_extra->user_category_id );
-    $level1   = $category;
-    if ( $category->parent_id ) {
-        $level1 = UserCategory::find( $category->parent_id );
+    $video = Video::userVideos($slug, $video_id)->first();
+    $user = User::whereUsername($slug)->first();
+    $category = UserCategory::find($user->user_extra->user_category_id);
+    $level1 = $category;
+    if ($category->parent_id) {
+        $level1 = UserCategory::find($category->parent_id);
         $level2 = $category;
-        $trail->push( $level1->name, route( 'directory', $level1->slug ) );
-        $trail->push( $level2->name, route( 'directory', [ $level1->slug, $level2->slug ] ) );
+        $trail->push($level1->name, route('directory', $level1->slug));
+        $trail->push($level2->name, route('directory', [$level1->slug, $level2->slug]));
     } else {
-        $trail->push( $level1->name, route( 'directory', $level1->slug ) );
+        $trail->push($level1->name, route('directory', $level1->slug));
     }
 
-    $trail->push( $user->name, route( 'directory_by_username', $slug ) );
-    if ( $video ) {
-        $trail->push( $video->title, route( 'directory_by_username', [ $slug, $video_id ] ) );
+    $trail->push($user->name, route('directory_by_username', $slug));
+    if ($video) {
+        $trail->push($video->title, route('directory_by_username', [$slug, $video_id]));
     }
-} );
+});
