b3e40248a49cb706b89ab8d344d1c012cfa2ee70
Faizan Abid <Faizanabid36@gmail.com>
2020-11-20 17:51:26 +0500

    asd

--- resources/js/antd/layout/index.js
+++ resources/js/antd/layout/index.js
@@ -215,15 +215,6 @@ class App extends React.Component {
                     {item.message}
                 </Menu.Item>
             })}
-            {/*<Menu.Item key="1" icon={<UserOutlined/>}>*/}
-            {/*    1st menu item*/}
-            {/*</Menu.Item>*/}
-            {/*<Menu.Item key="2" icon={<UserOutlined/>}>*/}
-            {/*    2nd menu item*/}
-            {/*</Menu.Item>*/}
-            {/*<Menu.Item key="3" icon={<UserOutlined/>}>*/}
-            {/*    3rd menu item*/}
-            {/*</Menu.Item>*/}
         </Menu>;
     }
 
