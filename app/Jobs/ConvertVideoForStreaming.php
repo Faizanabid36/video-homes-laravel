c1364fa26d9c3eec76996e65b91038a835b5ed8f
Essa Mamdani <mamdani.info@gmail.com>
2020-12-12 02:39:18 +0500

    fixed

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -14,67 +14,73 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
 class ConvertVideoForStreaming implements ShouldQueue {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    public $video, $width, $height, $bitrate, $update, $angle;
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+	public $video, $width, $height, $bitrate, $update, $angle;
 
-    /**
-     * Create a new job instance.
-     *
-     * @param Video $video
-     */
-    public function __construct( Video $video, $width, $height, $update = [], $angle = false, $bitrate = '500' ) {
-        //
-        $this->video   = $video;
-        $this->width   = $width;
-        $this->height  = $height;
-        $this->bitrate = $bitrate;
-        $this->update  = $update;
-        $this->angle   = $this->getAngle( $angle );
-    }
+	/**
+	 * Create a new job instance.
+	 *
+	 * @param Video $video
+	 */
+	public function __construct( Video $video, $width, $height, $update = array(), $angle = false, $bitrate = '500' ) {
+				$this->video = $video;
+		$this->width         = $width;
+		$this->height        = $height;
+		$this->bitrate       = $bitrate;
+		$this->update        = $update;
+		$this->angle         = $this->getAngle( $angle );
+	}
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle() {
-        // create a video format...
-        $lowBitrateFormat = (new X264('aac', 'libx264'))->setKiloBitrate(1000);
-//        $lowBitrateFormat->setInitialParameters(array('-acodec', 'copy'));
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle() {
+		// create a video format...
+		$lowBitrateFormat = ( new X264( 'aac', 'libx264' ) )->setKiloBitrate( 1000 );
+		// $lowBitrateFormat->setInitialParameters(array('-acodec', 'copy'));
 
-        $video = \FFMpeg::open($this->video->video_path);
-        Log::info("Essa Outside Angle", [$this->angle, $this->video->video_path, $video]);
-        if ($this->angle) {
-            Log::info("Essa Inside Angle", [$this->angle]);
-            $video->filters()->rotate($this->angle);
-        }
-        $video->filters()->pad(new Dimension($this->width, $this->height));
+		$video = \FFMpeg::open( $this->video->video_path );
+		Log::info( 'Essa Outside Angle', array( $this->angle, $this->video->video_path, $video ) );
+		if ( $this->angle ) {
+			Log::info( 'Essa Inside Angle', array( $this->angle ) );
+			$video->filters()->rotate( $this->angle );
+		}
 
-        $video->export()->inFormat($lowBitrateFormat)->save(getCleanFileName($this->video->video_path, "_{$this->height}p_converted.mp4"));
+		$video->filters()->pad( new Dimension( $this->width, $this->height ) );
+		// update the database so we know the convertion is done!
+		Log::info(
+			'This is some useful information.',
+			array(
+				'file_path' => $this->video->stream_path . "_{$this->height}p_converted.mp4",
+			)
+		);
 
-        // update the database so we know the convertion is done!
-        Log::info('This is some useful information.', [
-            'file_path' => $this->video->stream_path . "_{$this->height}p_converted.mp4"
-        ]);
-        Log::info('CHECKING UPDATE VARIABLE ON VIDEO', [
-            'title' => $this->video->title,
-            'update' => $this->update
-        ]);
+		$video->export()->inFormat( $lowBitrateFormat )->save( getCleanFileName( $this->video->video_path, "_{$this->height}p_converted.mp4" ) );
 
-        $this->video->update($this->update);
-    }
+		Log::info(
+			'CHECKING UPDATE VARIABLE ON VIDEO',
+			array(
+				'title'  => $this->video->title,
+				'update' => $this->update,
+			)
+		);
 
-    private function getAngle( $angle ) {
-        switch ( $angle ) {
-            case 270:
-                return RotateFilter::ROTATE_270;
-            case 180:
-                return RotateFilter::ROTATE_180;
-            case 90:
-                return RotateFilter::ROTATE_90;
-        }
-        return false;
-    }
+		$this->video->update( $this->update );
+	}
+
+	private function getAngle( $angle ) {
+		switch ( $angle ) {
+			case 270:
+				return RotateFilter::ROTATE_270;
+			case 180:
+				return RotateFilter::ROTATE_180;
+			case 90:
+				return RotateFilter::ROTATE_90;
+		}
+		return false;
+	}
 
 
 }
