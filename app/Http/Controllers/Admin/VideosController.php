103ec1d5c0f548da756ddfef91ad7e8172c89791
Essa Mamdani <mamdani.info@gmail.com>
2020-12-15 00:48:50 +0500

    fixed

--- app/Http/Controllers/Admin/VideosController.php
+++ app/Http/Controllers/Admin/VideosController.php
@@ -156,9 +156,10 @@ class VideosController extends Controller {
 	public function show( $id ) {
 		$video = Video::findOrFail( $id );
 		if ( $video->stream_path ) {
-			die( $video->stream_path );
+
 			$video->stream_path = str_replace( 'public/', '', $video->stream_path );
 		}
+		dd( $video->toArray() );
 		return view( 'admin.videos.show', compact( 'video' ) );
 	}
 
