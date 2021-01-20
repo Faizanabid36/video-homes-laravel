6a2442213217c70a4a064a88886548d29e93f78b
Essa Mamdani <mamdani.info@gmail.com>
2021-01-21 02:31:41 +0500

    Active users display

--- app/UserCategory.php
+++ app/UserCategory.php
@@ -5,54 +5,76 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class UserCategory extends Model {
-    //
 
-    protected $guarded = [];
+	protected $guarded = array();
 
-    protected $with = [ 'children', 'list' ];
+	protected $with = array( 'children', 'list' );
 
-    public function children() {
-        return $this->hasMany( $this, 'parent_id' );
-    }
+	public function children() {
+		return $this->hasMany( $this, 'parent_id' );
+	}
 
-    public function list() {
-        return $this->hasMany( UserExtra::class, 'user_category_id' );
-    }
-    public function parent() {
-        return $this->belongsTo( $this, 'parent_id')->without(['children','list']);
-    }
+	public function list() {
+		return $this->hasMany( UserExtra::class, 'user_category_id' );
+	}
+	public function parent() {
+		return $this->belongsTo( $this, 'parent_id' )->without( array( 'children', 'list' ) );
+	}
 
 
-    public function scopeGetCategories( $query, $level1 = null, $level2 = null ) {
-        return $query->when( ! $level1, function ( $query ) {
-            return $query->whereNull( 'parent_id' );
-        } )->when( $level1 && ! $level2, function ( $query ) use ( $level1 ) {
-            return $query->whereSlug( $level1 );
-        } )->when( $level2, function ( $query ) use ( $level2 ) {
-            return $query->whereSlug( $level2 );
-        } )->get();
+	public function scopeGetCategories( $query, $level1 = null, $level2 = null ) {
+		return $query->whereHas(
+			'parent',
+			function( $query ) {
+				return $query->whereActive( 1 );
+			}
+		)->when(
+			! $level1,
+			function ( $query ) {
+				return $query->whereNull( 'parent_id' );
+			}
+		)->when(
+			$level1 && ! $level2,
+			function ( $query ) use ( $level1 ) {
+				return $query->whereSlug( $level1 );
+			}
+		)->when(
+			$level2,
+			function ( $query ) use ( $level2 ) {
+				return $query->whereSlug( $level2 );
+			}
+		)->get();
 
-    }
+	}
 
-    public static function boot() {
-        parent::boot();
-        static::creating(function($user_category){
-            $user_category->slug = \Str::slug($user_category->name);
-        });
-        static::updating(function($user_category){
-            $user_category->slug = \Str::slug($user_category->name);
-        });
-        static::deleting( function ( $user_category ) { // before delete() method call this
-            $user_category->children()->each( function ( $child ) {
-                $child->delete(); // <-- direct deletion
-            } );
-        } );
-    }
+	public static function boot() {
+		parent::boot();
+		static::creating(
+			function( $user_category ) {
+				$user_category->slug = \Str::slug( $user_category->name );
+			}
+		);
+		static::updating(
+			function( $user_category ) {
+				$user_category->slug = \Str::slug( $user_category->name );
+			}
+		);
+		static::deleting(
+			function ( $user_category ) {
+				// before delete() method call this
+				$user_category->children()->each(
+					function ( $child ) {
+						$child->delete(); // <-- direct deletion
+					}
+				);
+			}
+		);
+	}
 
-    public function scopeLevelCategories( $query ) {
-        return $query->selectRaw( 'user_categories.*,uc1.name as parent_name' )
-                     ->leftJoin( 'user_categories as uc1', 'user_categories.parent_id', 'uc1.id' )
-                     ->get()->toArray();
-    }
+	public function scopeLevelCategories( $query ) {
+		return $query->selectRaw( 'user_categories.*,uc1.name as parent_name' )
+					 ->leftJoin( 'user_categories as uc1', 'user_categories.parent_id', 'uc1.id' )
+					 ->get()->toArray();
+	}
 
 }
